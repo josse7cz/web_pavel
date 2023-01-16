47b9265381a7ba1460f557fcf5b458f75de74b3b
Josef Janda <josefjanda@email.cz>
2023-01-16 11:05:34 +0100

    add ARIA tag ang Google analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,14 +44,14 @@ $(function() {
         $(window).scroll(function() {
         var scrollbarLocation = $(this).scrollTop();
 
+
         scrollLink.each(function() {
+            var sectionOffset = $(this.hash).offset().top - 73;
 
-          var sectionOffset = $(this.hash).offset().top - 73;
-
-          if ( sectionOffset <= scrollbarLocation ) {
-            $(this).parent().addClass('active');
-            $(this).parent().siblings().removeClass('active');
-          }
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
         });
     });    
     
